acd0ba739e96a182380f4f5c0f18f52330a10712
Abhisheknagrale <abhishek333roy@gmail.com>
2025-04-01 16:03:39 +0530

    handle-files

---
+++ CURD/src/app/handle-git/handle-git.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-handle-git',
+  standalone: true,
+  imports: [],
+  templateUrl: './handle-git.component.html',
+  styleUrl: './handle-git.component.css'
+})
+export class HandleGitComponent {
+
+}
